bb75b7639b4fd4cdb5101290bbae07b37700a268
Soraya Valle <svalleortega@gmail.com>
2020-04-04 18:42:39 +0200

    Add fonts and improve Landing component indentation

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -10,34 +10,34 @@ class Landing extends React.Component {
     return (
       <div className="Landing">
         <main id="mainindex" class="wrapperMain">
-    <div className="mainindex__container">
-        <div className="mainindex__container--logo">
-            <a className="mainindex__logo-container" href="#" title="">
+          <div className="mainindex__container">
+            <div className="mainindex__container--logo">
+              <a className="mainindex__logo-container" href="#" title="">
                 <img src={Logo} alt="Logo Awesome profile-cards"
                     title="Awesome profile-cards" />
-            </a>
-        </div>
-        <div className="mainindex__container--text">
+              </a>
+            </div>
+          <div className="mainindex__container--text">
             <h1 className="mainindex__container--title">Crea tu tarjeta de visita</h1>
             <p className="mainindex__container--prhase">Crea mejores contactos profesionales de forma fácil y cómoda</p>
-        </div>
-        <div className="mainindex__container--typeface">
+          </div>
+          <div className="mainindex__container--typeface">
             <div className="image-Designe">
-                <i className="far fa-object-ungroup"></i>
-                <h3 className="text-item">Diseña</h3>
+              <i className="far fa-object-ungroup"></i>
+              <h3 className="text-item">Diseña</h3>
+          </div>
+          <div className="image-complete">
+              <i className="far fa-keyboard fa-lg"></i>
+              <h3 className="text-item">rellena</h3>
+          </div>
+          <div className="image-share">
+              <i className="fas fa-share-alt"></i>
+              <h3 className="text-item">comparte</h3>
             </div>
-            <div className="image-complete">
-                <i className="far fa-keyboard fa-lg"></i>
-                <h3 className="text-item">rellena</h3>
-            </div>
-            <div className="image-share">
-                <i className="fas fa-share-alt"></i>
-                <h3 className="text-item">comparte</h3>
-            </div>
-        </div>
-        <a className="mainindex__button" href="./form.html#mainform.html">comenzar</a>
-    </div>
-</main>
+          </div>
+          <a className="mainindex__button" href="./form.html#mainform.html">comenzar</a>
+          </div>
+        </main>
       </div>
     );
   }
