d4b3be5fc4611f50a4beac5b41c4bc7cbe10b299
Elvira <elvi_ac94@hotmail.com>
2020-04-28 16:11:23 +0200

    add gif

--- src/components/demo/Square.js
+++ src/components/demo/Square.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import gif2 from '../../images/gif2.gif'
 
 const Square = () => {
   return (
-    <div>
-      Technology
+    <div className="demo__pages square">
+      <h2>Metodologías y tecnologías</h2>
+      <h3>Marco de trabajo: metodología Scrum</h3>
+      <div className="scrum__image--container">
+        <img src={gif2} />
+      </div>
     </div>
   )
 }
