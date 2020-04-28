8f0a2ce75975c76800eea1bf0003491376411f64
Elvira <elvi_ac94@hotmail.com>
2020-04-28 21:50:46 +0200

    add styles

--- src/components/demo/Square.js
+++ src/components/demo/Square.js
@@ -1,14 +1,49 @@
 import React from 'react';
-import gif2 from '../../images/gif2.gif'
+import gif2 from '../../images/gif2.gif';
+import ourtools from '../../images/ourtools.png';
+import react1 from '../../images/react1.png';
+import sass from '../../images/sass.png';
+import markdown from '../../images/markdown.png';
+import gityvs from '../../images/gityvs.png';
 
 const Square = () => {
   return (
-    <div className="demo__pages square">
-      <h2>Metodologías y tecnologías</h2>
-      <h3>Marco de trabajo: metodología Scrum</h3>
-      <div className="scrum__image--container">
-        <img src={gif2} />
+    <div className="demo__pages--square">
+      <div className="square__logo--container">
+        <img src={ourtools} alt="square-logo"/>
       </div>
+      <h3 className="scrum__subtitle--one">Marco de trabajo: metodología Agile y Scrum</h3>
+      <div className="scrum__container">
+        <div className="scrum__image--container">
+          <img src={gif2} alt="team-gif" />
+        </div>
+        <ul>
+          <li>Estudiando que se nos pide: Users Stories</li>
+          <li>Planificando que hacer en cada Sprint y así entregar valor</li>
+          <li>Autogestionandonos para cumplir con los objetivos marcados</li>
+          <li>Haciendo seguimiento diario </li>
+        </ul>
+      </div>
+      <div className="tecnologies__container">
+        <div className="react__image--container">
+          <img src={react1}/>
+        </div>
+        <div className="sass__image--container">
+          <img src={sass}/>
+        </div>
+        <div className="tecnologies__container--info">
+          <h3 className="scrum__subtitle--two" >Tecnologías y herramientas empleadas</h3>
+          <h4>React, SASS, GIT, Github, Markdown, JS</h4>
+          <p>Trello, Slack, Zoom </p>
+        </div>
+        <div className="markdown__image--container">
+          <img src={markdown}/>
+        </div>
+        <div className="gityvs__image--container">
+          <img src={gityvs}/>
+        </div>
+      </div>
+
     </div>
   )
 }
