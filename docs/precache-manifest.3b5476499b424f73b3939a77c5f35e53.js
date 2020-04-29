abc2d580bc796e3ae02ea0db866862bde42db186
Soraya Valle <svalleortega@gmail.com>
2020-04-29 08:25:05 +0200

    Create docs

---
+++ docs/precache-manifest.3b5476499b424f73b3939a77c5f35e53.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cd3e19ed92febf30bf2a3205318de2f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6d990da77ec2017647a7",
+    "url": "./static/css/main.6fd538ea.chunk.css"
+  },
+  {
+    "revision": "4d7678eaeb5451231ec2",
+    "url": "./static/js/2.ad60a9c3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.ad60a9c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d990da77ec2017647a7",
+    "url": "./static/js/main.d30d78f1.chunk.js"
+  },
+  {
+    "revision": "cbb7502546e28b5dbd02",
+    "url": "./static/js/runtime-main.5c2b7656.js"
+  },
+  {
+    "revision": "66d99d6291802050b8874ee3202bece3",
+    "url": "./static/media/arrow.66d99d62.png"
+  },
+  {
+    "revision": "9ac675e79881423ef99fb728c7abb966",
+    "url": "./static/media/elvira.9ac675e7.png"
+  },
+  {
+    "revision": "69fe1629e83c0d4e0950e8396ce2ba3a",
+    "url": "./static/media/esquema.69fe1629.png"
+  },
+  {
+    "revision": "d961b5fa84612b714cb31d7a71f7076d",
+    "url": "./static/media/gif2.d961b5fa.gif"
+  },
+  {
+    "revision": "267ff97244498d0f977fd4aa6e21c0a9",
+    "url": "./static/media/gif_code.267ff972.gif"
+  },
+  {
+    "revision": "16fbba9beaec372bb938d1bbedd8cd7f",
+    "url": "./static/media/giphy.16fbba9b.gif"
+  },
+  {
+    "revision": "79dd508d3522bc99def40651033502e3",
+    "url": "./static/media/github-c2.79dd508d.png"
+  },
+  {
+    "revision": "ce09a544e64c67cca3cabfa0d430aaf7",
+    "url": "./static/media/github-c3.ce09a544.png"
+  },
+  {
+    "revision": "2deef40bd9a8a933690f62479a8548d1",
+    "url": "./static/media/github.2deef40b.png"
+  },
+  {
+    "revision": "138143d48dbc4abd0baeb1e148e318a4",
+    "url": "./static/media/linkedin-c2.138143d4.png"
+  },
+  {
+    "revision": "146fa187757628fdfb133c91be6d41c8",
+    "url": "./static/media/linkedin-c3.146fa187.png"
+  },
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "./static/media/logo_awesome.0118e311.svg"
+  },
+  {
+    "revision": "47bebcde93b883faafa0b4d0b221e82d",
+    "url": "./static/media/mail-c2.47bebcde.png"
+  },
+  {
+    "revision": "4945ceed8dafac3784d485869a22b5c4",
+    "url": "./static/media/mail-c3.4945ceed.png"
+  },
+  {
+    "revision": "ec85be63d790deca8c93dccc05b65305",
+    "url": "./static/media/maria.ec85be63.png"
+  },
+  {
+    "revision": "a7f9f74be7a345a543110398c80c4bad",
+    "url": "./static/media/ourcode.a7f9f74b.png"
+  },
+  {
+    "revision": "d2817ce20862099c2b6a7558a48f3c6b",
+    "url": "./static/media/ourstructure.d2817ce2.png"
+  },
+  {
+    "revision": "813ee774db616cb1f3e62f39f30f67a7",
+    "url": "./static/media/ourteam.813ee774.png"
+  },
+  {
+    "revision": "245f66393852e0a9cd5ecacb3c25e533",
+    "url": "./static/media/reactlogo.245f6639.png"
+  },
+  {
+    "revision": "0d1f44bfb777e424e993b6facb8b9c6f",
+    "url": "./static/media/romi.0d1f44bf.png"
+  },
+  {
+    "revision": "dc68f2b0109faaab7b1ed6c1ce85204d",
+    "url": "./static/media/soraya.dc68f2b0.png"
+  },
+  {
+    "revision": "842d2488ebe0f7284f1ecfe9fa6834b9",
+    "url": "./static/media/teamname.842d2488.png"
+  },
+  {
+    "revision": "5f3c748c0186fd10f24db9b98b0d51e3",
+    "url": "./static/media/teamwork.5f3c748c.png"
+  }
+]);
